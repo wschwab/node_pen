5b442f984b5421434e08690fc723dc8228d27c79
William <w.s.schwab4@gmail.com>
2019-03-04 16:21:23 +0200

    added liking and following

--- twitter-bot/follow.js
+++ twitter-bot/follow.js
@@ -17,16 +17,13 @@ T.get('search/tweets', params, function(err, data, response) {
   if(!err){
     //loop through returned tweets
     for(let i=0; i<data.statuses.length; i++){
-      //get tweet id
-      let id = { id: data.statuses[i].id_str }
-      //try to favorite
-      T.post('favorites/create', id, function(err, response){
+      let screen_name = data.statuses[i].user.screen_name;
+
+      T.post('friendships/create', {screen_name}, function(err, res){
         if(err){
-          console.log(err[0].message);
+          console.log(err);
         }else{
-          let username = response.user.screen_name;
-          let tweetId = response.id_str;
-          console.log('Favorited: ', 'https://twitter.com/${username}/status/$tweetId')
+          console.log(screen_name, ': **FOLLOWED**');
         }
       });
     }
